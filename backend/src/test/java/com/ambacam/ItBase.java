3a6f5edad479518c25afb315f6c422f740e74fe8
MentalistHB <hervalnganya@yahoo.fr>
2018-06-10 15:00:10 +0200

    Integration test configured. .gitignore set up

--- backend/src/test/java/com/ambacam/ItBase.java
+++ backend/src/test/java/com/ambacam/ItBase.java
@@ -3,15 +3,14 @@ package com.ambacam;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AmbacamBackendApplicationTests extends AbstractJUnit4SpringContextTests {
-
+public class ItBase {
 
 	@Test
 	public void contextLoads() {
 	}
+
 }
