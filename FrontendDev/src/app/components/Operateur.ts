6291fb9f1adb0d8a732b4b56781777407842d789
Kevin Tchagwo <yannicktchag@live.fr>
2018-07-01 13:31:46 +0200

    no message

---
+++ FrontendDev/src/app/components/Operateur.ts
@@ -0,0 +1,15 @@
+/**
+ * New typescript file
+ */
+export class Operateur {
+
+  constructor(
+    public id: number,
+    public nom: string,
+    public prenom: string,
+    public roles: any,
+    public username: string,
+    public passwort: string
+   ) {}
+
+}
