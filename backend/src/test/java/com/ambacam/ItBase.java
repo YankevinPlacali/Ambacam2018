a97b250e98b997ab037a5f094d0788c4f013567d
MentalistHB <hervalnganya@yahoo.fr>
2018-12-09 13:08:57 +0100

    Bug Fix: Tests made independent from pre-loading data when starting

--- backend/src/test/java/com/ambacam/ItBase.java
+++ backend/src/test/java/com/ambacam/ItBase.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ambacam.configuration.AppSettings;
@@ -52,6 +53,7 @@ import io.restassured.specification.RequestSpecification;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ItBase {
 
 	protected String AUTH_COLLECTION_PATH = "/oauth/token?grant_type=password&username=%s&password=%s";
